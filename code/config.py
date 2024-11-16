a99adafea4b8887025b1e46f2f3d4e12dcc5cdf4
ranomia <rarurararurara@gmail.com>
2024-11-16 15:23:34 +0900

    [fix] kaggle側の特徴量作成後のデータを追加
    config上のパス変更した。

--- code/config.py
+++ code/config.py
@@ -10,10 +10,8 @@ class Config:
         self._cv_seed = 150
         self._train_file_path = '../data/input/train.csv'
         self._test_file_path = '../data/input/test.csv'
-        # self._train_preprocessed_file_path = '../data/intermediate/train_preprocessed.pickle'
-        # self._test_preprocessed_file_path = '../data/intermediate/test_preprocessed.pickle'
-        self._train_preprocessed_file_path = '../data/input/train.csv'
-        self._test_preprocessed_file_path = '../data/input/test.csv'
+        self._train_preprocessed_file_path = '../data/intermediate/train.pickle'
+        self._test_preprocessed_file_path = '../data/intermediate/test.pickle'
         self._target_column = 'sii'
         self._group_column = None # group無の場合はNoneを指定
 
